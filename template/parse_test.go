4bc279e3e237d55005baaf1b5f9f4fe0aba6f786
Adrien Delorme <adrien.delorme@icloud.com>
2018-10-08 10:49:58 +0200

    add hcl parse tests from json parse tests

--- template/parse_test.go
+++ template/parse_test.go
@@ -30,12 +30,34 @@ func TestParse(t *testing.T) {
 			false,
 		},
 		{
+			"parse-basic.hcl",
+			&Template{
+				Builders: map[string]*Builder{
+					"something": {
+						Name: "something",
+						Type: "something",
+					},
+				},
+			},
+			false,
+		},
+		{
 			"parse-builder-no-type.json",
 			nil,
 			true,
 		},
 		{
+			"parse-builder-no-type.hcl",
+			nil,
+			true,
+		},
+		{
 			"parse-builder-repeat.json",
+			nil,
+			true,
+		},
+		{
+			"parse-builder-repeat.hcl",
 			nil,
 			true,
 		},
@@ -54,9 +76,32 @@ func TestParse(t *testing.T) {
 			},
 			false,
 		},
+		{
+			"parse-provisioner-basic.hcl",
+			&Template{
+				Provisioners: []*Provisioner{
+					{
+						Type: "something",
+					},
+				},
+			},
+			false,
+		},
 
 		{
 			"parse-provisioner-pause-before.json",
+			&Template{
+				Provisioners: []*Provisioner{
+					{
+						Type:        "something",
+						PauseBefore: 1 * time.Second,
+					},
+				},
+			},
+			false,
+		},
+		{
+			"parse-provisioner-pause-before.hcl",
 			&Template{
 				Provisioners: []*Provisioner{
 					{
@@ -82,9 +127,37 @@ func TestParse(t *testing.T) {
 			},
 			false,
 		},
+		{
+			"parse-provisioner-only.hcl",
+			&Template{
+				Provisioners: []*Provisioner{
+					{
+						Type: "something",
+						OnlyExcept: OnlyExcept{
+							Only: []string{"foo"},
+						},
+					},
+				},
+			},
+			false,
+		},
 
 		{
 			"parse-provisioner-except.json",
+			&Template{
+				Provisioners: []*Provisioner{
+					{
+						Type: "something",
+						OnlyExcept: OnlyExcept{
+							Except: []string{"foo"},
+						},
+					},
+				},
+			},
+			false,
+		},
+		{
+			"parse-provisioner-except.hcl",
 			&Template{
 				Provisioners: []*Provisioner{
 					{
@@ -114,13 +187,45 @@ func TestParse(t *testing.T) {
 			},
 			false,
 		},
+		{
+			"parse-provisioner-override.hcl",
+			&Template{
+				Provisioners: []*Provisioner{
+					{
+						Type: "something",
+						Override: []map[string]interface{}{{
+							"foo": []map[string]interface{}{{
+								"bar": "baz",
+							}},
+						}},
+					},
+				},
+			},
+			false,
+		},
 
 		{
 			"parse-provisioner-no-type.json",
 			nil,
 			true,
 		},
+		{
+			"parse-provisioner-no-type.hcl",
+			nil,
+			true,
+		},
 
+		{
+			"parse-variable-default.json",
+			&Template{
+				Variables: map[string]*Variable{
+					"foo": {
+						Default: "foo",
+					},
+				},
+			},
+			false,
+		},
 		{
 			"parse-variable-default.json",
 			&Template{
@@ -144,9 +249,36 @@ func TestParse(t *testing.T) {
 			},
 			false,
 		},
+		{
+			"parse-variable-required.hcl",
+			&Template{
+				Variables: map[string]*Variable{
+					"foo": {
+						Required: true,
+					},
+				},
+			},
+			false,
+		},
 
 		{
 			"parse-pp-basic.json",
+			&Template{
+				PostProcessors: [][]*PostProcessor{
+					{
+						{
+							Type: "foo",
+							Config: map[string]interface{}{
+								"foo": "bar",
+							},
+						},
+					},
+				},
+			},
+			false,
+		},
+		{
+			"parse-pp-basic.hcl",
 			&Template{
 				PostProcessors: [][]*PostProcessor{
 					{
@@ -176,9 +308,39 @@ func TestParse(t *testing.T) {
 			},
 			false,
 		},
+		{
+			"parse-pp-keep.hcl",
+			&Template{
+				PostProcessors: [][]*PostProcessor{
+					{
+						{
+							Type:              "foo",
+							KeepInputArtifact: true,
+						},
+					},
+				},
+			},
+			false,
+		},
 
 		{
 			"parse-pp-only.json",
+			&Template{
+				PostProcessors: [][]*PostProcessor{
+					{
+						{
+							Type: "foo",
+							OnlyExcept: OnlyExcept{
+								Only: []string{"bar"},
+							},
+						},
+					},
+				},
+			},
+			false,
+		},
+		{
+			"parse-pp-only.hcl",
 			&Template{
 				PostProcessors: [][]*PostProcessor{
 					{
@@ -210,9 +372,38 @@ func TestParse(t *testing.T) {
 			},
 			false,
 		},
+		{
+			"parse-pp-except.hcl",
+			&Template{
+				PostProcessors: [][]*PostProcessor{
+					{
+						{
+							Type: "foo",
+							OnlyExcept: OnlyExcept{
+								Except: []string{"bar"},
+							},
+						},
+					},
+				},
+			},
+			false,
+		},
 
 		{
 			"parse-pp-string.json",
+			&Template{
+				PostProcessors: [][]*PostProcessor{
+					{
+						{
+							Type: "foo",
+						},
+					},
+				},
+			},
+			false,
+		},
+		{
+			"parse-pp-string.hcl",
 			&Template{
 				PostProcessors: [][]*PostProcessor{
 					{
@@ -238,9 +429,40 @@ func TestParse(t *testing.T) {
 			},
 			false,
 		},
+		{
+			"parse-pp-map.hcl",
+			&Template{
+				PostProcessors: [][]*PostProcessor{
+					{
+						{
+							Type: "foo",
+						},
+					},
+				},
+			},
+			false,
+		},
 
 		{
 			"parse-pp-slice.json",
+			&Template{
+				PostProcessors: [][]*PostProcessor{
+					{
+						{
+							Type: "foo",
+						},
+					},
+					{
+						{
+							Type: "bar",
+						},
+					},
+				},
+			},
+			false,
+		},
+		{
+			"parse-pp-slice.hcl",
 			&Template{
 				PostProcessors: [][]*PostProcessor{
 					{
@@ -276,15 +498,45 @@ func TestParse(t *testing.T) {
 			},
 			false,
 		},
+		{
+			"parse-pp-multi.hcl",
+			&Template{
+				PostProcessors: [][]*PostProcessor{
+					{
+						{
+							Type: "foo",
+						},
+					},
+					{
+						{
+							Type: "bar",
+						},
+					},
+				},
+			},
+			false,
+		},
 
 		{
 			"parse-pp-no-type.json",
 			nil,
 			true,
 		},
+		{
+			"parse-pp-no-type.hcl",
+			nil,
+			true,
+		},
 
 		{
 			"parse-description.json",
+			&Template{
+				Description: "foo",
+			},
+			false,
+		},
+		{
+			"parse-description.hcl",
 			&Template{
 				Description: "foo",
 			},
@@ -298,9 +550,25 @@ func TestParse(t *testing.T) {
 			},
 			false,
 		},
+		{
+			"parse-min-version.hcl",
+			&Template{
+				MinVersion: "1.2",
+			},
+			false,
+		},
 
 		{
 			"parse-push.json",
+			&Template{
+				Push: Push{
+					Name: "foo",
+				},
+			},
+			false,
+		},
+		{
+			"parse-push.hcl",
 			&Template{
 				Push: Push{
 					Name: "foo",
@@ -321,13 +589,25 @@ func TestParse(t *testing.T) {
 			},
 			false,
 		},
+		{
+			"parse-comment.hcl",
+			&Template{
+				Builders: map[string]*Builder{
+					"something": {
+						Name: "something",
+						Type: "something",
+					},
+				},
+			},
+			false,
+		},
 	}
 
 	for i, tc := range cases {
 		path, _ := filepath.Abs(fixtureDir(tc.File))
 		tpl, err := ParseFile(fixtureDir(tc.File))
 		if (err != nil) != tc.Err {
-			t.Errorf("err: %s", err)
+			t.Errorf("[%d]bad: %s. err: %s", i, tc.File, err)
 			continue
 		}
 
@@ -344,15 +624,17 @@ func TestParse(t *testing.T) {
 }
 
 func TestParse_contents(t *testing.T) {
-	tpl, err := ParseFile(fixtureDir("parse-contents.json"))
-	if err != nil {
-		t.Fatalf("err: %s", err)
-	}
+	for _, f := range []string{"parse-contents.json", "parse-contents.hcl"} {
+		tpl, err := ParseFile(fixtureDir(f))
+		if err != nil {
+			t.Fatalf("err: %s", err)
+		}
 
-	actual := strings.TrimSpace(string(tpl.RawContents))
-	expected := `{"builders":[{"type":"test"}]}`
-	if actual != expected {
-		t.Fatalf("bad: %s\n\n%s", actual, expected)
+		actual := strings.TrimSpace(string(tpl.RawContents))
+		expected := `{"builders":[{"type":"test"}]}`
+		if actual != expected {
+			t.Fatalf("bad: %s\n\n%s", actual, expected)
+		}
 	}
 }
 
